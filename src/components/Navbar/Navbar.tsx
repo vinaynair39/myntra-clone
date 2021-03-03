063e4cdca1bc906865486cfa80b28ba8f3bd03ed
Vinay Nair <vnnair39@gmail.com>
2021-03-04 02:19:41 +0530

    added navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
-import styles from "./Navbar.module.scss";
-import { ReactComponent as Logo } from "assets/logo.svg";
+import { Link } from "react-router-dom";
+import logo from "assets/logo.png";
 import { ReactComponent as Bag } from "assets/bag.svg";
 import { ReactComponent as Heart } from "assets/heart.svg";
-import { ReactComponent as Profile } from "assets/Profile.svg";
+import { ReactComponent as Profile } from "assets/profile.svg";
+import { ReactComponent as Search } from "assets/search.svg";
 
-import { Link } from "react-router-dom";
+import styles from "./Navbar.module.scss";
 
 interface Props {}
 const Navbar: React.FC<Props> = ({}) => {
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
-        <Logo />
+        <img src={logo} alt="logo" />
       </div>
       <div className={styles.links}>
         <Link to="/shop/men">MEN</Link>
@@ -22,10 +23,26 @@ const Navbar: React.FC<Props> = ({}) => {
         <Link to="/shop/offers">OFFERS</Link>
       </div>
       <div className={styles.searchbar}>
+        <div className={styles.searchbarIcon}>
+          <Search />
+        </div>
         <input type="text" placeholder="Search for products, brands and more" />
       </div>
 
-      <div className={styles.profiles}></div>
+      <div className={styles.profiles}>
+        <div className={styles.profileItem}>
+          <Profile />
+          <p>Profile</p>
+        </div>
+        <div className={styles.profileItem}>
+          <Heart />
+          <p>Wishlist</p>
+        </div>
+        <div className={styles.profileItem}>
+          <Bag />
+          <p>Bag</p>
+        </div>
+      </div>
     </div>
   );
 };
